44dd13330f4f6ea7311b82333cc505051265a2b5
Robin <robin@robinsingh.co>
2020-03-31 01:53:31 +0000

    .

--- question_generation/mod_interact.py
+++ question_generation/mod_interact.py
@@ -223,7 +223,7 @@ def question_generation(_input):
     model.to(args.device)
     model.eval()
 
-    return {"break":"point"}
+    #return {"break":"point"}
     #data = get_positional_dataset_from_file(tokenizer, args.filename)
     data = get_positional_dataset_from_file(tokenizer, output)
     final_output_dict = {
